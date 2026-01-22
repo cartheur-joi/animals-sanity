5416fb69296a31448eff4865e885200a484c720f
C.A.Tucker <cartheur@pm.me>
2026-01-22 12:49:36 +0100

    Implement feature

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Boagaphish;
+using Boagaphish.Analytics;
 using Cartheur.Presents;
 
 class Program
@@ -12,6 +12,9 @@ class Program
     static string TrainingDataFiles { get; set; }
     static string FileName { get; set; }
     static bool UseFile { get; set; }
+    static DetectEmotion DetectEmotion { get; set; }
+    static string OutputDetectionFileName = "listening_";
+
     static async Task Main()
     {
         Console.WriteLine("in here");
@@ -21,37 +24,37 @@ class Program
         classifier.LoadData(Configuration.PathToTrainingData);
         var path = Path.Combine(LoaderPaths.ActiveRuntime, LoaderPaths.SavePath, FileName + ".wav");
         Console.WriteLine(Terminal.StartBashProcess("arecord -vv -r 16000 -c 1 -f S16_LE -d 1 " + path));
-        // Set the recording duration
-            //RecordingDuration = 1000;
-            // if (!UseFile)
-            // {
-            //     VoiceRecorder = new Recorder();
-            //     await VoiceRecorder.Record(ReturnRecordingFilePath("recorded"), RecordingDuration);
-            //     Console.WriteLine("Started recording...");
-            //     VoiceRecorder.RecordingFinished += RecordingEvent;
-            // }
-            Console.WriteLine("Getting the file for analysis...");
-            string audioFilePath = ReturnRecordingFilePath("recorded");
-            // Send over for the file for analysis and return the approximated emotion.
-            try
-            {
-                DetectEmotion = new Boagaphish.Analytics.DetectEmotion(ReturnRecordingFilePath(OutputDetectionFileName), "linux");
-            }
-            catch (Exception ex)
-            {
+        // Set the recording duration.
+        RecordingDuration = 1000;
+        if (!UseFile)
+        {
+            VoiceRecorder = new Recorder();
+            await VoiceRecorder.Record(ReturnRecordingFilePath("recorded"), RecordingDuration);
+            Console.WriteLine("Started recording...");
+            VoiceRecorder.RecordingFinished += RecordingEvent;
+        }
+        Console.WriteLine("Getting the file for analysis...");
+        string audioFilePath = ReturnRecordingFilePath("recorded");
+        // Send over for the file for analysis and return the approximated emotion.
+        try
+        {
+            DetectEmotion = new DetectEmotion(ReturnRecordingFilePath(OutputDetectionFileName), "linux");
+        }
+        catch (Exception ex)
+        {
 
-            }
+        }
 
-            //Thread.Sleep(2000);
-            // Load the data
+        //Thread.Sleep(2000);
+        // Load the data
 
-            // Train the model
-            classifier.TrainModel();
-            // Predict the emotion
-            var emotion = classifier.PredictEmotion(FileName);
+        // Train the model
+        classifier.TrainModel();
+        // Predict the emotion
+        var emotion = classifier.PredictEmotion(FileName);
 
-            // Display the classification result
-            Console.WriteLine($"The predicted emotion is: {emotion}");
+        // Display the classification result
+        Console.WriteLine($"The predicted emotion is: {emotion}");
         await Task.CompletedTask;
     }
     static string ReturnRecordingFilePath(string filename)
